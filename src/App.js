4f5eb9327b15073946231334050732072b92bdf3
Ryan <17rcp@queensu.ca>
2021-01-14 02:30:09 -0500

    Queries are finally working and displaying movies
    end of day2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,73 @@
-import logo from './shoppiesLogo.png';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './shoppiesLogo.png';
+import voteBanner from './voteNow.png'
+import MovieList from './MovieList';
+import Search from './Search';
 
-function App() {
-  return (
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&type=movie&apikey=6c8d3168`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <body>
-        <div className="headerUnderline" >
-          
-          </div>
-      
+        <div className="headerUnderline"></div>
+        <div> <img src={voteBanner} className="voteNow" alt="voteNowLogo" /></div>
+        <div>
+          <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+        </div>
+        <div className='row'>
+				<MovieList movies={movies} />
+			</div>
       </body>
     </div>
-  );
-}
+	);
+};
 
 export default App;
+
+
+// import logo from './shoppiesLogo.png';
+// import voteBanner from './voteNow.png'
+// import './App.css';
+// import Search from './Search';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//       </header>
+//       <body>
+//         <div className="headerUnderline"></div>
+//         <div> <img src={voteBanner} className="voteNow" alt="voteNowLogo" /></div>
+//         <div>
+//           <Search/>
+//         </div>
+        
+//       </body>
+//     </div>
+//   );
+// }
+
+// export default App;
