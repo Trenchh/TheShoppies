f7f3e12f25ccf27ee1939708921ac9531df0fafb
Ryan <17rcp@queensu.ca>
2021-01-16 15:17:27 -0500

    Added 'Trending' Nominations. Improved layout/styling.
    Site is populated when entered, nominations now center aligned.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import NominationList from './NominationList';
 import Search from './Search';
 import AddNomination from './AddNomination';
 import RemoveNomination from './RemoveNomination';
+import initialDetails from './InitialDetails';
+
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
@@ -33,7 +35,7 @@ const App = () => {
 	const removeNominationsFromList = (movie) => {
 		const newNominationList = nominations.filter(
 			(nomination) => nomination.imdbID !== movie.imdbID
-		);	
+		);
 		setNominations(newNominationList);
 	};
 
@@ -43,38 +45,42 @@ const App = () => {
 
 
 	return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <body>
-        <div className="headerUnderline"></div>
-        <div className="voteNow"> <img src={voteBanner}  alt="voteNowLogo" /></div>
-        <div>
-          <Search searchValue={searchValue} setSearchValue={setSearchValue} />
-        </div>
-        <div className='container-fluid movie-row'>
-			{searchValue !== ''
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+			</header>
+			<body>
+				<div className="headerUnderline"></div>
+				<div className="voteNow"> <img src={voteBanner} alt="voteNowLogo" /></div>
+				<div>
+					<Search searchValue={searchValue} setSearchValue={setSearchValue} />
+				</div>
+				<div className='container-fluid movie-row'>
+					{searchValue !== ''
 						? <div className="resultsHeading">
 							<h1>Results for "{searchValue}"</h1>
 						</div>
 						: <div className="resultsHeading">
-							<h1>Top Contenders</h1>
+							<h1>Trending</h1>
 						</div>
-			}
-
-			<div className='row movieListSpacing'>
-				<MovieList movies={movies} nominations={nominations} favouriteComponent={AddNomination} handleNominationClick={addNominationsToList}/>
-			</div>
-			<div className="nomHeading">
-				<h1>Your Nominations</h1>
-			</div>
-			<div className='row movieListSpacing nomSection'>
-				<NominationList movies={nominations} favouriteComponent={RemoveNomination} handleNominationClick={removeNominationsFromList}/>
-			</div>
+					}
+					{searchValue !== '' && nominations.length < 5
+						? <div className='row movieListSpacing'>
+							<MovieList movies={movies} nominations={nominations} favouriteComponent={AddNomination} handleNominationClick={addNominationsToList} />
+						</div>
+						: <div className='row movieListSpacing'>
+							<MovieList movies={initialDetails} nominations={nominations} favouriteComponent={AddNomination} handleNominationClick={addNominationsToList} />
+						</div>
+					}
+					<div className="nomHeading">
+						<h1>Your Nominations</h1>
+					</div>
+					<div className='row movieListSpacing nomSection'>
+						<NominationList movies={nominations} favouriteComponent={RemoveNomination} handleNominationClick={removeNominationsFromList} />
+					</div>
+				</div>
+			</body>
 		</div>
-      </body>
-    </div>
 	);
 };
 
---
+++ src/InitialDetails.js
@@ -0,0 +1,90 @@
+const initialDetails = [
+    {
+        Title: "Fight Club",
+        Year: "1999",
+        imdbID: "tt0137523",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+      },
+      {
+        Title: "Interstellar",
+        Year: "2014",
+        imdbID: "tt0816692",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+      },
+      {
+        Title: "Scott Pilgrim vs. the World",
+        Year: "2010",
+        imdbID: "tt0446029",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BMTkwNTczNTMyOF5BMl5BanBnXkFtZTcwNzUxOTUyMw@@._V1_SX300.jpg"
+      },
+      {
+        Title: "The Devil Wears Prada",
+        Year: "2006",
+        imdbID: "tt0458352",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BZjQ3ZTIzOTItMGNjNC00MWRmLWJlMGEtMjJmMDM5ZDIzZGM3XkEyXkFqcGdeQXVyMTkzODUwNzk@._V1_SX300.jpg"
+      },
+      {
+        "Title": "Akira",
+        "Year": "1988",
+        "imdbID": "tt0094625",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BM2ZiZTk1ODgtMTZkNS00NTYxLWIxZTUtNWExZGYwZTRjODViXkEyXkFqcGdeQXVyMTE2MzA3MDM@._V1_SX300.jpg"
+      },
+      {
+        Title: "Spider-Man: Into the Spider-Verse",
+        Year: "2018",
+        imdbID: "tt4633694",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_SX300.jpg"
+      },
+      {
+        "Title": "Charlie and the Chocolate Factory",
+        "Year": "2005",
+        "imdbID": "tt0367594",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNjcxMjg1Njg2NF5BMl5BanBnXkFtZTcwMjQ4NzMzMw@@._V1_SX300.jpg"
+      },
+      {
+        "Title": "Doctor Strange",
+        "Year": "2016",
+        "imdbID": "tt1211837",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg"
+      },
+      {
+        "Title": "Alita: Battle Angel",
+        "Year": "2019",
+        "imdbID": "tt0437086",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTQzYWYwYjctY2JhZS00NTYzLTllM2UtZWY5ZTk0NmYwYzIyXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_SX300.jpg"
+      },
+      {
+        "Title": "Mad Max: Fury Road",
+        "Year": "2015",
+        "imdbID": "tt1392190",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BN2EwM2I5OWMtMGQyMi00Zjg1LWJkNTctZTdjYTA4OGUwZjMyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+      },
+      {
+        "Title": "The Room",
+        "Year": "2003",
+        "imdbID": "tt0368226",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYjEzN2FlYmYtNDkwMC00NGFkLWE5ODctYmE5NmYxNzE2MmRiXkEyXkFqcGdeQXVyMjMwODc5Mw@@._V1_SX300.jpg"
+      },
+      {
+        "Title": "Spy Kids 3-D: Game Over",
+        "Year": "2003",
+        "imdbID": "tt0338459",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTI4MTQyNTUzMF5BMl5BanBnXkFtZTcwNzE2MDAwMQ@@._V1_SX300.jpg"
+      }
+  ];
+
+  export default initialDetails;
+
+      
